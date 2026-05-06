570b29539d861524c9b91ea108b9bf74859fbc70
ForeverIce <a0981667889@gmail.com>
2026-05-06 20:45:07 +0800

    feat:PerformanceRecord

--- src/main/java/template/model/PerformanceRecord.java
+++ src/main/java/template/model/PerformanceRecord.java
@@ -1,21 +1,18 @@
 package template.model;
 
 public class PerformanceRecord {
-    // 使用 private 隱私化欄位，這是良好的開發習慣
-    private int id;
+
     private String gameName;
     private double avgFps;
     private double cpuTemp;
     private double gpuTemp;
     private double cpuUsage;
     private double gpuUsage;
-    protected String monitorName;
-    public PerformanceRecord() {
-    }
+    private String monitorName;
 
+    public PerformanceRecord() {}
 
-    // 建構子 (Constructor)
-    public PerformanceRecord(String gameName, double avgFps, double cpuTemp, double gpuTemp, double cpuUsage, double gpuUsage,String monitorName) {
+    public PerformanceRecord(String gameName, double avgFps, double cpuTemp, double gpuTemp, double cpuUsage, double gpuUsage, String monitorName) {
         this.gameName = gameName;
         this.avgFps = avgFps;
         this.cpuTemp = cpuTemp;
@@ -25,70 +22,36 @@ public class PerformanceRecord {
         this.monitorName = monitorName;
     }
 
-    // --- Getter 和 Setter 開始 ---
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getGameName() {
-        return gameName;
-    }
-
+    // Getter & Setter 務必包含 monitorName[cite: 7]
+    public String getMonitorName() { return monitorName; }
+    public void setMonitorName(String monitorName) { this.monitorName = monitorName; }
+    public double getAvgFps() { return avgFps; }
+    public double getCpuTemp() { return cpuTemp; }
+    public double getGpuTemp() { return gpuTemp; }
+    public double getCpuUsage() { return cpuUsage; }
+    public double getGpuUsage() { return gpuUsage; }
     public void setGameName(String gameName) {
         this.gameName = gameName;
     }
 
-    public double getAvgFps() {
-        return avgFps;
-    }
-
     public void setAvgFps(double avgFps) {
-        // 你可以在這裡加入邏輯，例如：FPS 不可能小於 0
-        if (avgFps >= 0) {
-            this.avgFps = avgFps;
-        }
-    }
-
-    public double getCpuTemp() {
-        return cpuTemp;
+        this.avgFps = avgFps;
     }
 
     public void setCpuTemp(double cpuTemp) {
         this.cpuTemp = cpuTemp;
     }
 
-    public double getGpuTemp() {
-        return gpuTemp;
-    }
-
     public void setGpuTemp(double gpuTemp) {
         this.gpuTemp = gpuTemp;
     }
 
-    public double getCpuUsage() {
-        return cpuUsage;
-    }
-
     public void setCpuUsage(double cpuUsage) {
-        // 防呆邏輯：占用率最高就是 100%
-        this.cpuUsage = Math.min(cpuUsage, 100.0);
-    }
-
-    public double getGpuUsage() {
-        return gpuUsage;
+        this.cpuUsage = cpuUsage;
     }
 
     public void setGpuUsage(double gpuUsage) {
-        this.gpuUsage = Math.min(gpuUsage, 100.0);
+        this.gpuUsage = gpuUsage;
     }
-    
-    public String getMonitorName() {return monitorName;}
 
-    public void setMonitorName(String monitorName) {
-    }
 }
